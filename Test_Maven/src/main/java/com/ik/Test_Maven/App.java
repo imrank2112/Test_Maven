8e4c94f8f5339a51d8f2844d0f7aafda766d6ebd
imrank2112 <imrank2112@gmail.com>
2015-08-13 11:19:06 +0100

    Adding Test_Maven Project TO GIT

---
+++ Test_Maven/src/main/java/com/ik/Test_Maven/App.java
@@ -0,0 +1,14 @@
+package com.ik.Test_Maven;
+
+
+ 
+public class App 
+{
+	
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+      
+    }
+}
---
+++ Test_Maven/src/test/java/com/ik/Test_Maven/AppTest.java
@@ -0,0 +1,44 @@
+package com.ik.Test_Maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+	static WebDriver driver =null;
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+    	  driver = new FirefoxDriver();
+        assertTrue( true );
+    }
+}
